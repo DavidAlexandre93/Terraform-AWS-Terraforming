3d992be27d7023b30f61872d3cfb8b3f3d954071
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2022-03-27 00:10:06 -0300

    Add files via upload

---
+++ devops/credentials.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+
+    access_key = "sua_access_key"
+
+    secret_key = "sua_secret_key"
+
+    region = "sa-east-1"
+
+}
---
+++ devops/webserver-teste.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "web" {
+
+    ami = "ami-d1a7ccbd"
+
+    instance_type = "t2.micro"
+
+    associate_public_ip_address = true
+
+    tags {
+
+        Name = "Web Server Teste"
+
+    }
+
+    subnet_id = "subnet-1aa9cf80"
+
+}
